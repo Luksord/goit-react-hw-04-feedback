4a2d858ab6308a017237874242567fb8bbc0b371
Łukasz Wątorski <watorski.info@gmail.com>
2024-05-02 22:34:31 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Statistics } from './Statistics/Statistics';
@@ -13,56 +13,41 @@ const appStyles = {
   // color: '#010101',
 };
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     good:0,
-  //     neutral: 0,
-  //     bad: 0,
-  //   };
-  // }
+export function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  // export class App extends Component {
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       good: 0,
+  //       neutral: 0,
+  //       bad: 0,
+  //     };
+  //   }
 
-  totalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-    //     const total = this.state.good + this.state.neutral + this.state.bad;
-    //     return total;
-  };
-
-  positivePercentageFeedback = () => {
-    return Math.round((this.state.good / this.totalFeedback()) * 100);
-    //     const positivePercentage = Math.round(
-    //       (this.state.good / this.totalFeedback()) * 100
-    //     );
-    //     return positivePercentage;
-  };
-
-  handleFeedback = event => {
+  const handleFeedback = event => {
     const { name } = event.target;
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [name]: prevFeedback[name] + 1,
+    }));
   };
   //   handleFeedback = event => {
-  //     this.setState(prevState => ({
-  //       [event]: prevState[event] + 1,
-  //     }));
+  //     const { name } = event.target;
+  //     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
   //   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.totalFeedback();
-    const positivePercentage = this.positivePercentageFeedback();
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+  const positivePercentage = Math.round((good / total) * 100);
 
-    return (
-      <>
+  return (
+    <>
+      <div style={appStyles}>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.handleFeedback}
+            options={feedback}
+            onLeaveFeedback={handleFeedback}
           />
           {total === 0 ? (
             <Notification message="There is no feedback" />
@@ -76,7 +61,59 @@ export class App extends Component {
             />
           )}
         </Section>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
+
+// export class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       good: 0,
+//       neutral: 0,
+//       bad: 0,
+//     };
+//   }
+
+//   totalFeedback = () => {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   };
+
+//   positivePercentageFeedback = () => {
+//     return Math.round((this.state.good / this.totalFeedback()) * 100);
+//   };
+
+//   handleFeedback = event => {
+//     const { name } = event.target;
+//     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.totalFeedback();
+//     const positivePercentage = this.positivePercentageFeedback();
+
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={this.state}
+//             onLeaveFeedback={this.handleFeedback}
+//           />
+//           {total === 0 ? (
+//             <Notification message="There is no feedback" />
+//           ) : (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
